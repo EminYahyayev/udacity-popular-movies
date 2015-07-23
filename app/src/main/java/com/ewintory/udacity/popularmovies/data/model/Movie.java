527567e216e098dbe60ab1aef93cf34df39ea4b5
ewintory <ewintory@gmail.com>
2015-07-23 17:24:58 +0500

    Local DB with favorite movies.

--- app/src/main/java/com/ewintory/udacity/popularmovies/data/model/Movie.java
+++ app/src/main/java/com/ewintory/udacity/popularmovies/data/model/Movie.java
@@ -1,85 +1,78 @@
 package com.ewintory.udacity.popularmovies.data.model;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Parcel;
 import android.os.Parcelable;
 
+import com.ewintory.udacity.popularmovies.data.db.Db;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+import com.squareup.sqlbrite.SqlBrite;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+
+import rx.functions.Func1;
+
+import static com.squareup.sqlbrite.SqlBrite.Query;
 
 
-// This annotation will trigger annotation processor
-// Which will generate type mapping code in compile time,
-// You just need to link it in your code.
-//@StorIOSQLiteType(table = "movies")
 public final class Movie implements Parcelable {
+    public static final String TABLE = "movie";
+
+    public static final String ID = "_id";
+    public static final String TITLE = "title";
+    public static final String GENRE_IDS = "genre_ids";
+    public static final String OVERVIEW = "overview";
+    public static final String POPULARITY = "popularity";
+    public static final String VOTE_COUNT = "vote_count";
+    public static final String VOTE_AVERAGE = "vote_average";
+    public static final String FAVORED = "favored";
+    public static final String POSTER_PATH = "poster_path";
+    public static final String BACKDROP_PATH = "backdrop_path";
 
     @Expose
-    //@StorIOSQLiteColumn(name = "_id", key = true)
-            Integer id;
+    long id;
 
-    @Expose
-    @SerializedName("backdrop_path")
-    //@StorIOSQLiteColumn(name = "backdrop_path")
-            String backdropPath;
-
-    @Expose
-    @SerializedName("genre_ids")
+    @Expose @SerializedName("genre_ids")
     List<Integer> genreIds = new ArrayList<>();
 
     @Expose
-    @SerializedName("original_language")
-    //@StorIOSQLiteColumn(name = "original_language")
-            String originalLanguage;
+    String overview;
+
+    @Expose @SerializedName("release_date")
+    String releaseDate;
+
+    @Expose @SerializedName("poster_path")
+    String posterPath;
+
+    @Expose @SerializedName("backdrop_path")
+    String backdropPath;
 
     @Expose
-    @SerializedName("original_title")
-    //@StorIOSQLiteColumn(name = "original_title")
-            String originalTitle;
+    double popularity;
 
     @Expose
-    //@StorIOSQLiteColumn(name = "overview")
-            String overview;
+    String title;
 
-    @Expose
-    @SerializedName("release_date")
-    //@StorIOSQLiteColumn(name = "release_date")
-            String releaseDate;
+    @Expose @SerializedName("vote_average")
+    double voteAverage;
 
-    @Expose
-    @SerializedName("poster_path")
-    //@StorIOSQLiteColumn(name = "poster_path")
-            String posterPath;
+    @Expose @SerializedName("vote_count")
+    long voteCount;
 
-    @Expose
-    Double popularity;
-
-    @Expose
-    //@StorIOSQLiteColumn(name = "title")
-            String title;
-
-    @Expose
-    Boolean video;
-
-    @Expose
-    @SerializedName("vote_average")
-    //@StorIOSQLiteColumn(name = "voteAverage")
-            Double voteAverage;
-
-    @Expose
-    @SerializedName("vote_count")
-    //@StorIOSQLiteColumn(name = "voteCount")
-            Integer voteCount;
+    boolean favored = false;
 
     public Movie() {}
 
-    public Integer getId() {
+    public long getId() {
         return id;
     }
 
-    public Movie setId(Integer id) {
+    public Movie setId(long id) {
         this.id = id;
         return this;
     }
@@ -99,24 +92,6 @@ public final class Movie implements Parcelable {
 
     public Movie setGenreIds(List<Integer> genreIds) {
         this.genreIds = genreIds;
-        return this;
-    }
-
-    public String getOriginalLanguage() {
-        return originalLanguage;
-    }
-
-    public Movie setOriginalLanguage(String originalLanguage) {
-        this.originalLanguage = originalLanguage;
-        return this;
-    }
-
-    public String getOriginalTitle() {
-        return originalTitle;
-    }
-
-    public Movie setOriginalTitle(String originalTitle) {
-        this.originalTitle = originalTitle;
         return this;
     }
 
@@ -147,11 +122,11 @@ public final class Movie implements Parcelable {
         return this;
     }
 
-    public Double getPopularity() {
+    public double getPopularity() {
         return popularity;
     }
 
-    public Movie setPopularity(Double popularity) {
+    public Movie setPopularity(double popularity) {
         this.popularity = popularity;
         return this;
     }
@@ -165,30 +140,30 @@ public final class Movie implements Parcelable {
         return this;
     }
 
-    public Boolean getVideo() {
-        return video;
-    }
-
-    public Movie setVideo(Boolean video) {
-        this.video = video;
-        return this;
-    }
-
-    public Double getVoteAverage() {
+    public double getVoteAverage() {
         return voteAverage;
     }
 
-    public Movie setVoteAverage(Double voteAverage) {
+    public Movie setVoteAverage(double voteAverage) {
         this.voteAverage = voteAverage;
         return this;
     }
 
-    public Integer getVoteCount() {
+    public long getVoteCount() {
         return voteCount;
     }
 
-    public Movie setVoteCount(Integer voteCount) {
+    public Movie setVoteCount(long voteCount) {
         this.voteCount = voteCount;
+        return this;
+    }
+
+    public boolean isFavored() {
+        return favored;
+    }
+
+    public Movie setFavored(boolean favored) {
+        this.favored = favored;
         return this;
     }
 
@@ -197,108 +172,161 @@ public final class Movie implements Parcelable {
                 "backdropPath='" + backdropPath + '\'' +
                 ", genreIds=" + genreIds +
                 ", id=" + id +
-                ", originalLanguage='" + originalLanguage + '\'' +
-                ", originalTitle='" + originalTitle + '\'' +
                 ", overview='" + overview + '\'' +
                 ", releaseDate='" + releaseDate + '\'' +
                 ", posterPath='" + posterPath + '\'' +
                 ", popularity=" + popularity +
                 ", title='" + title + '\'' +
-                ", video=" + video +
                 ", voteAverage=" + voteAverage +
                 ", voteCount=" + voteCount +
                 '}';
     }
 
-    public static final class Response {
-
-        @Expose
-        private Integer page;
-
-        @Expose
-        @SerializedName("total_pages")
-        private Integer totalPages;
-
-        @Expose
-        @SerializedName("total_results")
-        private Integer totalMovies;
-
-        @SerializedName("results")
-        private List<Movie> movies = new ArrayList<>();
-
-        public Integer getPage() {
-            return page;
+    public static final Func1<SqlBrite.Query, List<Movie>> MAP = new Func1<Query, List<Movie>>() {
+        @Override public List<Movie> call(Query query) {
+            Cursor cursor = query.run();
+            try {
+                List<Movie> values = new ArrayList<>(cursor.getCount());
+                while (cursor.moveToNext()) {
+                    values.add(new Movie()
+                            .setId(Db.getLong(cursor, ID))
+                            .setTitle(Db.getString(cursor, TITLE))
+                            .setPosterPath(Db.getString(cursor, POSTER_PATH))
+                            .setBackdropPath(Db.getString(cursor, BACKDROP_PATH))
+                            .setFavored(Db.getBoolean(cursor, FAVORED))
+                            .setPopularity(Db.getDouble(cursor, POPULARITY))
+                            .setVoteCount(Db.getInt(cursor, VOTE_COUNT))
+                            .setVoteAverage(Db.getDouble(cursor, VOTE_AVERAGE))
+                            .setOverview(Db.getString(cursor, OVERVIEW)));
+                }
+                return values;
+            } finally {
+                cursor.close();
+            }
         }
+    };
 
-        public Response setPage(Integer page) {
-            this.page = page;
+    public static final Func1<SqlBrite.Query, Set<Long>> ID_MAP = new Func1<Query, Set<Long>>() {
+        @Override public Set<Long> call(Query query) {
+            Cursor cursor = query.run();
+            try {
+                Set<Long> idSet = new HashSet<>(cursor.getCount());
+                while (cursor.moveToNext()) {
+                    idSet.add(Db.getLong(cursor, ID));
+                }
+                return idSet;
+            } finally {
+                cursor.close();
+            }
+        }
+    };
+
+    public static final class Builder {
+        private final ContentValues values = new ContentValues();
+
+        public Builder id(long id) {
+            values.put(ID, id);
             return this;
         }
 
-        public Integer getTotalPages() {
-            return totalPages;
-        }
-
-        public Response setTotalPages(Integer totalPages) {
-            this.totalPages = totalPages;
+        public Builder title(String title) {
+            values.put(TITLE, title);
             return this;
         }
 
-        public Integer getTotalMovies() {
-            return totalMovies;
-        }
+//        public Builder listId(List<Long> genreIds) {
+//            values.put(GENRE_IDS, genreIds);
+//            return this;
+//        }
 
-        public Response setTotalMovies(Integer totalMovies) {
-            this.totalMovies = totalMovies;
+        public Builder overview(String overview) {
+            values.put(OVERVIEW, overview);
             return this;
         }
 
-        public List<Movie> getMovies() {
-            return movies;
+        public Builder backdropPath(String backdropPath) {
+            values.put(BACKDROP_PATH, backdropPath);
+            return this;
         }
 
-        public Response setMovies(List<Movie> movies) {
-            this.movies = movies;
+        public Builder posterPath(String posterPath) {
+            values.put(POSTER_PATH, posterPath);
             return this;
+        }
+
+        public Builder voteCount(long voteCount) {
+            values.put(VOTE_COUNT, voteCount);
+            return this;
+        }
+
+        public Builder voteAverage(double voteCount) {
+            values.put(VOTE_AVERAGE, voteCount);
+            return this;
+        }
+
+        public Builder popularity(double popularity) {
+            values.put(POPULARITY, popularity);
+            return this;
+        }
+
+        public Builder favored(boolean favored) {
+            values.put(FAVORED, favored);
+            return this;
+        }
+
+        public Builder movie(Movie movie) {
+            return id(movie.getId())
+                    .title(movie.getTitle())
+                    .overview(movie.getOverview())
+                    .backdropPath(movie.getBackdropPath())
+                    .posterPath(movie.getPosterPath())
+                    .popularity(movie.getPopularity())
+                    .voteCount(movie.getVoteCount())
+                    .voteAverage(movie.getVoteAverage())
+                    .favored(movie.isFavored());
+        }
+
+        public ContentValues build() {
+            return values; // TODO defensive copy?
         }
     }
+
+
+    // --------------------------------------------------------------------------------------
+
 
     @Override public int describeContents() { return 0; }
 
     @Override public void writeToParcel(Parcel dest, int flags) {
-        dest.writeValue(this.id);
-        dest.writeString(this.backdropPath);
+        dest.writeLong(this.id);
         dest.writeList(this.genreIds);
-        dest.writeString(this.originalLanguage);
-        dest.writeString(this.originalTitle);
         dest.writeString(this.overview);
         dest.writeString(this.releaseDate);
         dest.writeString(this.posterPath);
-        dest.writeValue(this.popularity);
+        dest.writeString(this.backdropPath);
+        dest.writeDouble(this.popularity);
         dest.writeString(this.title);
-        dest.writeValue(this.video);
-        dest.writeValue(this.voteAverage);
-        dest.writeValue(this.voteCount);
+        dest.writeDouble(this.voteAverage);
+        dest.writeLong(this.voteCount);
+        dest.writeByte(favored ? (byte) 1 : (byte) 0);
     }
 
     protected Movie(Parcel in) {
-        this.id = (Integer) in.readValue(Integer.class.getClassLoader());
-        this.backdropPath = in.readString();
+        this.id = in.readLong();
         this.genreIds = new ArrayList<Integer>();
         in.readList(this.genreIds, List.class.getClassLoader());
-        this.originalLanguage = in.readString();
-        this.originalTitle = in.readString();
         this.overview = in.readString();
         this.releaseDate = in.readString();
         this.posterPath = in.readString();
-        this.popularity = (Double) in.readValue(Double.class.getClassLoader());
+        this.backdropPath = in.readString();
+        this.popularity = in.readDouble();
         this.title = in.readString();
-        this.video = (Boolean) in.readValue(Boolean.class.getClassLoader());
-        this.voteAverage = (Double) in.readValue(Double.class.getClassLoader());
-        this.voteCount = (Integer) in.readValue(Integer.class.getClassLoader());
+        this.voteAverage = in.readDouble();
+        this.voteCount = in.readLong();
+        this.favored = in.readByte() != 0;
     }
 
-    public static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+    public static final Creator<Movie> CREATOR = new Creator<Movie>() {
         public Movie createFromParcel(Parcel source) {return new Movie(source);}
 
         public Movie[] newArray(int size) {return new Movie[size];}
